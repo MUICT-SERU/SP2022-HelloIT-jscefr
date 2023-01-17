17da9349099c8bff1f0f230964c7553d79af51f3
klemonade <lemon.thanakrit@gmail.com>
2023-01-17 06:07:05 +0000

    [format]: delete comment

--- antlr_packages/python/JscefrParser.py
+++ antlr_packages/python/JscefrParser.py
@@ -13,32 +13,6 @@ class JscefrParser( JavaScriptParser ):
 
         localctx = JavaScriptParser.ProgramContext(self, self._ctx, self.state)
         self.enterRule(localctx, 0, self.RULE_program)
-        # try:
-        #     self.enterOuterAlt(localctx, 1)
-        #     self.state = 155
-        #     self._errHandler.sync(self)
-        #     la_ = self._interp.adaptivePredict(self._input,0,self._ctx)
-        #     if la_ == 1:
-        #         self.state = 154
-        #         self.match(JavaScriptParser.HashBangLine)
-
-
-        #     self.state = 158
-        #     self._errHandler.sync(self)
-        #     la_ = self._interp.adaptivePredict(self._input,1,self._ctx)
-        #     if la_ == 1:
-        #         self.state = 157
-        #         self.sourceElements()
-
-
-        #     self.state = 160
-        #     self.match(JavaScriptParser.EOF)
-        # except RecognitionException as re:
-        #     localctx.exception = re
-        #     self._errHandler.reportError(self, re)
-        #     self._errHandler.recover(self, re)
-        # finally:
-        #     self.exitRule()
         self.enterOuterAlt(localctx, 1)
         self.state = 155
         self._errHandler.sync(self)
