cedace6194b1790b4c622834be79cba5e4716846
klemonade <lemon.thanakrit@gmail.com>
2023-03-08 08:21:31 +0000

    added C2_components

--- statistical-test/rq3_dataset_creator.py
+++ statistical-test/rq3_dataset_creator.py
@@ -4,7 +4,7 @@ columns = ['Competency Levels', 'Code Constructs', 'Files']
 levels = ['A1', 'A2', 'B1', 'B2', 'C1', 'C2']
 rows = levels + ['Total']
 modules = {'react': {'report name': 'react', 'test files location': '/__tests__/'}, 'svelte': {'report name': 'kit', 'test files location': '/test/'}, 'next_js': {'report name': 'next.js', 'test files location': '/test/'}}
-C2_files = {'test': [], 'non-test': []}
+C2_files = {'test': {}, 'non-test': {}}
 path = 'statistical-test/datasets/rq3'
 
 for module in modules:
@@ -31,7 +31,7 @@ for module in modules:
                     files[i] += 1
 
                     if levels[i] == 'C2':
-                        C2_files[('non-' if not test else '') + 'test'].append(f)
+                        C2_files[('non-' if not test else '') + 'test'][f] = test_or_non_test_files[f]['Class']
 
         with open(f'{path}/{module + ("_non" if not test else "")}_test.csv', 'w') as f:
             writer = csv.writer(f)
@@ -47,7 +47,7 @@ for test in [True, False]:
     files = [0] * 7
 
     for fname in all_files:
-        if not ('non' in fname) == test:
+        if not (('non' in fname) == test) and not ('all' in fname) and fname != 'C2_components.csv':
 
             with open(f'{path}/{fname}', 'r') as f:
                 reader = csv.reader(f)
@@ -63,3 +63,22 @@ for test in [True, False]:
         writer.writerow(columns)
         for i in range(len(rows)):
             writer.writerow([rows[i], code_constructs[i], files[i]])
+
+dictionary = json.load(open('dictionary_converter/dict.json'))
+C2_classes = [code_cons['Class'] for code_cons in dictionary if code_cons['Level'] == 'C2']
+
+columns2 = ['Test', 'File Name', 'Code Construct', 'Number of Code Constructs']
+
+C2_data = []
+
+for group in C2_files:
+    for each_file in C2_files[group]:
+        for code_cons in C2_files[group][each_file]:
+            if code_cons in C2_classes:
+                C2_data.append([('False' if 'non' in group else 'True'), each_file, code_cons, C2_files[group][each_file][code_cons]])
+
+with open(f'{path}/C2_components.csv', 'w') as f:
+    writer = csv.writer(f)
+    writer.writerow(columns2)
+    for row in C2_data:
+        writer.writerow(row)
